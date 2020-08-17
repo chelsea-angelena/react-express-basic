6cc028c6dab486f180eac1c971879afae32c34cf
Chelsea Angelena <chelsea.angelena@gmail.com>
2020-08-17 14:38:37 -0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ServerConnect from './ServerConnect'
+
+function App() {
+  return (
+    <div className="App">
+<ServerConnect/>
+    </div>
+  );
+}
+
+export default App;
